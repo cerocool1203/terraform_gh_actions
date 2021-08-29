0467988a5592b7c4f4f42d26e317e7a9b458ea36
unknown <adm.jalfaro@australiansuper.com>
2021-08-29 17:01:28 +1000

    added tf files

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_resource_group" "cero" {
+  name     = "WEBAPP-RG-01"
+  location = "australiaeast"
+}
+
+resource "azurerm_app_service_plan" "cero" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.cero.location
+  resource_group_name = azurerm_resource_group.cero.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+#App Service 
+resource "azurerm_app_service" "cero_prod" {
+  name                = var.app_service_name_prefix
+  location            = azurerm_resource_group.cero.location
+  resource_group_name = azurerm_resource_group.cero.name
+  app_service_plan_id = azurerm_app_service_plan.cero.id
+
+  site_config {
+    linux_fx_version = "NODE|12-lts"
+    app_command_line = "npx http-server"
+  }
+}
+
+#App Service Staging Slot 
+resource "azurerm_app_service_slot" "cero" {
+  name                = "staging"
+  app_service_name    = azurerm_app_service.cero_prod.name
+  location            = azurerm_resource_group.cero.location
+  resource_group_name = azurerm_resource_group.cero.name
+  app_service_plan_id = azurerm_app_service_plan.cero.id
+
+  site_config {
+    linux_fx_version = "NODE|12-lts"
+    scm_type         = "VSTSRM"
+    app_command_line = "npx http-server"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,13 @@
+output "appservice_name_prod" {
+  value       = azurerm_app_service.cero_prod.name
+  description = "The App Service name for the prod environment"
+}
+output "website_hostname_prod" {
+  value       = azurerm_app_service.cero_prod.default_site_hostname
+  description = "The hostname of the website in the prod environment"
+}
+
+output "website_hostname_staging" {
+  value       = azurerm_app_service_slot.cero.default_site_hostname
+  description = "The hostname of the website in the Staging slot name"
+}
